ee40a76adacf91be07cc05a5f76933dc7d1b2ff0
alex <enna@mailbox.org>
2019-10-30 08:54:36 +0100

    Ignores

--- start.sh
+++ start.sh
@@ -3,6 +3,4 @@
 export KB_USERNAME=enna
 export KB_PAPERKEY="manual lyrics dog jacket initial success ethics about grant radio joy duck metal"
 
-
-node target/scala-2.12/scalajs-bundler/main/mybot-opt.js
-
+node math.js
