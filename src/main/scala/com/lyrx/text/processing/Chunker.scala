7c1665002850f1ebb9dbe9d80045c333e595a5c5
alex <enna@mailbox.org>
2019-11-17 16:01:32 +0100

    reading files line by line in node.js

--- src/main/scala/com/lyrx/text/processing/Chunker.scala
+++ src/main/scala/com/lyrx/text/processing/Chunker.scala
@@ -1,6 +1,5 @@
 package com.lyrx.text.processing
 
-
 import typings.node.{Buffer, process, fsMod => fs, readlineMod => readline}
 import typings.node.NodeJS.{ErrnoException, WritableStream}
 import typings.node.fsMod.ReadStream
@@ -9,39 +8,62 @@ import typings.node.readlineMod.Interface
 import scala.scalajs.js
 import js.annotation.{JSExport, JSExportTopLevel}
 import js.|
-
-
 @JSExportTopLevel("Chunker")
 object Main extends Chunker {
 
+  type ArrayGet = Array[String] => Unit
+  type HeaderLevel = String => Int
+
+
+  val may = "/Users/alex/git/texte/projects/lyrxgenerator/src/main/resources/books/KarlMay"
+
+  implicit val headerLevel: HeaderLevel = (line) => {
+    val s = line.trim
+
+    if (s.startsWith("####"))
+      4
+    else if (s.startsWith("###"))
+      3
+    else if (s.startsWith("##"))
+      2
+    else if (s.startsWith("#"))
+      1
+    else
+      0
+  }
+
+
   @JSExport
   def initt(): Unit = {
-    read(fs.createReadStream("build.sbt"),_.foreach(println))
+    toSections(fs.createReadStream(s"${may}/satanundischarioti.md"),
+         //  _.foreach(println)
+         map => {println(map.size)})
   }
+
 }
 
+case class Section(level: Int)
 
 trait Chunker {
 
-  def read(readStream: ReadStream,onClosed:Array[String] => Unit) = {
+  def read(readStream: ReadStream, onClosed: Main.ArrayGet) = {
     val interface: Interface = readline.createInterface(readStream)
-    var  seq:Array[String] = Array()
-    interface.on("line",(s)=>{seq = (seq :+ s.toString)})
-    interface.on("close",(e)=>onClosed(seq))
+    var seq: Array[String] = Array()
+    interface.on("line", (s) => { seq = (seq :+ s.toString) })
+    interface.on("close", (e) => onClosed(seq))
   }
 
-
-  def toPars(lines: Array[String]): Map[Int, Array[String]] = {
-    var counter = 0;
-    def isHeader(s: String) = s.trim.startsWith("#")
-
-    lines.groupBy[Int]((line: String) => {
-      if (isHeader(line))
-        counter = counter + 1
-      counter
+  def toSections(
+      readStream: ReadStream,
+      cb: Map[Section, Array[String]] => Unit
+  )(implicit headerLevel: Main.HeaderLevel) =
+    read(readStream, lines => {
+      var counter = 0;
+      cb(lines.groupBy[Section]((line: String) => {
+        if (headerLevel(line) > 0)
+          counter = counter + 1
+        Section(counter)
+      }))
     })
-
-  }
-
 
 }
