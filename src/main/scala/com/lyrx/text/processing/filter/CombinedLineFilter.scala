582883e016aff60c122e136b3285d0fac331cf9a
alex <enna@mailbox.org>
2019-11-26 15:02:41 +0100

    Filtering

--- src/main/scala/com/lyrx/text/processing/filter/CombinedLineFilter.scala
+++ src/main/scala/com/lyrx/text/processing/filter/CombinedLineFilter.scala
@@ -2,78 +2,34 @@ package com.lyrx.text.processing.filter
 
 import com.lyrx.text.processing.Types.Lines
 import com.lyrx.text.processing.filter.Types.FilterFunc
-import typings.node
-import node.{fsMod => fs, readlineMod => readline}
-import scala.concurrent.Future
 
+import scala.concurrent.{ExecutionContext, Future}
 
-trait CombinedLineFilter  extends MarkerFilter with LineNumberFilter {
-  val filters:Seq[FilterFunc]
+trait CombinedLineFilter extends MarkerFilter with LineNumberFilter {
+  val filters: Seq[FilterFunc]
 
-  def filterLines(in: Seq[String]): Seq[String] = filters.
-    foldLeft(in: Seq[String])((r,t)=>t(r))
+  def filterLines(in: Seq[String]): Seq[String] =
+    filters.foldLeft(in: Seq[String])((r, t) => t(r))
 
+  def :+(filter: FilterFunc): CombinedLineFilter =
+    new CombinedLineFilterImpl(filters :+ filter)
 
-  def :+(filter:FilterFunc): CombinedLineFilter = new
-      CombinedLineFilterImpl(filters :+ filter)
-
-  def :+(from:Int,to:Int): CombinedLineFilter = new
-      CombinedLineFilterImpl(filters :+ filterLineNumbers(from,to) _ )
-  def :+(marker:String): CombinedLineFilter = new
-      CombinedLineFilterImpl(filters :+ filterMarks(marker) _ )
-
-
+  def :+(from: Int, to: Int): CombinedLineFilter =
+    new CombinedLineFilterImpl(filters :+ filterLineNumbers(from, to) _)
+  def :+(marker: String): CombinedLineFilter =
+    new CombinedLineFilterImpl(filters :+ filterMarks(marker) _)
 
 }
 
-object  CombinedLineFilterImpl{
-
+object CombinedLineFilterImpl extends LinesFromFile {
 
   def apply() = new CombinedLineFilterImpl(Seq())
 
-  /*
-    def lineFilter(
-                    file:String,
-                    from:Int,
-                    to:Int)(implicit
-                  executionContext: ExecutionContext
-    ): Future[Seq[String]] = lines(fs.
-      createReadStream(file)).map(lines=>
-      new LineNumberFilterImpl(from,to).filterLines(lines))
-
-
-
-
-    def filterLines(
-                    lines:Lines,
-                    from:Int,
-                    to:Int)(implicit
-                            executionContext: ExecutionContext
-                  )=
-      new LineNumberFilterImpl(from,to).filterLines(lines)
-
-
-
-
-   */
-
-
-
-
-
-  def lines(readStream: fs.ReadStream): Future[Lines] = {
-    val interface: readline.Interface = readline.createInterface(readStream)
-    var seq: Seq[String] = Seq()
-    val promise = concurrent.Promise[Lines]()
-    interface.on("line", (s) => { seq = (seq :+ s.toString) })
-    interface.on("close", (e) => {
-      promise.success(seq)
-      ()
-    })
-    promise.future
-  }
-}
-
-class CombinedLineFilterImpl(override val filters:Seq[FilterFunc]) extends CombinedLineFilter{
+  def filterFile(file: String, filter: FilterFunc)(
+      implicit
+      executionContext: ExecutionContext) = fromFile(file).map(filter(_))
 
 }
+
+class CombinedLineFilterImpl(override val filters: Seq[FilterFunc])
+    extends CombinedLineFilter {}
---
+++ src/main/scala/com/lyrx/text/processing/filter/LinesFromFile.scala
@@ -0,0 +1,25 @@
+package com.lyrx.text.processing.filter
+
+import com.lyrx.text.processing.Types.Lines
+
+import scala.concurrent.Future
+import typings.node
+import node.{fsMod => fs, readlineMod => readline}
+
+trait LinesFromFile {
+
+  def fromFile(file: String) = lines(fs.createReadStream(file))
+
+  def lines(readStream: fs.ReadStream): Future[Lines] = {
+    val interface: readline.Interface = readline.createInterface(readStream)
+    var seq: Seq[String] = Seq()
+    val promise = concurrent.Promise[Lines]()
+    interface.on("line", (s) => { seq = (seq :+ s.toString) })
+    interface.on("close", (e) => {
+      promise.success(seq)
+      ()
+    })
+    promise.future
+  }
+
+}
