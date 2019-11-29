404457d2a44250a02536997585f49a6157f91408
alex <enna@mailbox.org>
2019-11-29 20:15:58 +0100

    filtering

--- src/main/scala/com/lyrx/text/processing/Taker.scala
+++ src/main/scala/com/lyrx/text/processing/Taker.scala
@@ -1,16 +1,12 @@
 package com.lyrx.text.processing
 
-import com.lyrx.text.processing.Types.{Lines, Page, PageMap}
-import com.lyrx.text.processing.filter.LinesFromFile
+import com.lyrx.text.processing.Types.{Lines, PageMap}
+import com.lyrx.text.processing.filter.{LineFilter, LinesFromFile}
 import typings.mkdirp.mkdirpMod.{Made, ^ => mkdirp}
 import typings.node
 import node.NodeJS.ErrnoException
-import typings.node.childUnderscoreProcessMod.spawn
-import typings.node.fsMod.PathLike
 
-import scala.concurrent.{ExecutionContext, Future, Promise}
-import scala.scalajs.js
-import scala.scalajs.js.|
+import scala.concurrent.{ExecutionContext, Future}
 case class Taking(
     mdInputPathOpt: Option[String] = None,
     idOpt: Option[String] = None,
@@ -21,7 +17,8 @@ case class Taking(
     mdOutputPathOpt: Option[String] = None,
     mdFrags: Iterable[String] = Seq(),
     htmlFrags: Iterable[String] = Seq(),
-    sectionNum: Int = 0
+    sectionNum: Int = 0,
+    filterOpt:Option[Lines=>Lines] = None
 )
 
 object Taker {
@@ -33,6 +30,24 @@ class Taker(override val taking: Taking)
     with Grouping2
     with Writer
     with Generator {
+
+  def  withFilter(f:Lines=>Lines) =
+  new Taker(  taking.copy(filterOpt = Some(f)))
+
+  def filter() = taking.linesOpt.
+    flatMap(lines=>taking.
+      filterOpt.
+      map(filter=>
+      new Taker(taking.copy(
+        linesOpt=Some(
+          filter(lines)))))).getOrElse(Taker.this)
+
+  def writeLines(file:String)(
+    implicit executionContext: ExecutionContext)=
+    writeAFile(file).
+      map(_.map(s=>Taker.this)).
+      getOrElse(Future{Taker.this})
+
 
   def mdPath(s: String) =
     new Taker(taking.copy(mdInputPathOpt = Some(s)))
--- src/main/scala/com/lyrx/text/processing/filter/LineFilter.scala
+++ src/main/scala/com/lyrx/text/processing/filter/LineFilter.scala
@@ -1,7 +1,7 @@
 package com.lyrx.text.processing.filter
 
-trait LineFilter[T] {
+trait LineFilter {
 
-  def filterLines(params:T)(in: Seq[String]): Seq[String]
+  def filterLines(in: Seq[String]): Seq[String]
 
 }
