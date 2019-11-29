5044eacecc0324fe1994618a8ab1f6b7ff121391
alex <enna@mailbox.org>
2019-11-29 21:10:43 +0100

    Wittgenstein

--- src/main/scala/com/lyrx/text/processing/filter/Filters.scala
+++ src/main/scala/com/lyrx/text/processing/filter/Filters.scala
@@ -5,6 +5,7 @@ import com.lyrx.text.processing.Types.Lines
 object Filters {
 
   val R1 = """:\s+(.*)""".r
+  val R11 = """\s+(.*)""".r
   val R2 = """\[(\d+)\].*""".r
   val R3 = """\[(\d+\.\d)\].*""".r
   val R4 = """\[(\d+\.\d\d)\].*""".r
@@ -14,17 +15,18 @@ object Filters {
 
 
 
-  val tractatus:Lines=>Lines =(lines)=>
+  val tractatus:Lines=>Lines =(lines: Seq[String])=>
     lines.map(line=>line match{
-      case R1(aline) => aline
-      case R2(num)   => s"# ${num} #"
-      case R3(num)   => s"## ${num} ##"
-      case R4(num)   => s"### ${num} ###"
-      case R5(num)   => s"#### ${num} ####"
-      case R6(num)   => s"##### ${num} #####"
-      case R7(num)   => s"###### ${num} ######"
-      case _ => line
-    })
+      case R1(aline:String) => Seq[String]("\n",aline)
+      case R11(aline) => Seq[String](aline)
+      case R2(num)   => Seq[String](s"# ${num} #")
+      case R3(num)   => Seq[String](s"## ${num} ##")
+      case R4(num)   => Seq[String](s"### ${num} ###")
+      case R5(num)   => Seq[String](s"#### ${num} ####")
+      case R6(num)   => Seq[String](s"##### ${num} #####")
+      case R7(num)   => Seq[String](s"###### ${num} ######")
+      case _ => Seq[String](line)
+    }).flatten
 
 
 }
