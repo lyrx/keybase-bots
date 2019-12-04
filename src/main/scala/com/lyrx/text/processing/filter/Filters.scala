ae6b2ca4d2b5b8e6147e84a1aa202f7b8904b591
alex <enna@mailbox.org>
2019-12-04 09:17:14 +0100

    Markdown filter

--- src/main/scala/com/lyrx/text/processing/filter/Filters.scala
+++ src/main/scala/com/lyrx/text/processing/filter/Filters.scala
@@ -13,26 +13,38 @@ object Filters {
   val R6 = """\[(\d+\.\d\d\d\d)\].*""".r
   val R7 = """\[(\d+\.\d\d\d\d\d)\].*""".r
 
+  val tractatus: Lines => Lines = (lines: Seq[String]) =>
+    lines
+      .map(line =>
+        line match {
+          case R1(aline: String) => Seq[String]("\n", aline)
+          case R11(aline)        => Seq[String](aline)
+          case R2(num)           => Seq[String](s"# ${num} #")
+          case R3(num)           => Seq[String](s"## ${num} ##")
+          case R4(num)           => Seq[String](s"### ${num} ###")
+          case R5(num)           => Seq[String](s"#### ${num} ####")
+          case R6(num)           => Seq[String](s"##### ${num} #####")
+          case R7(num)           => Seq[String](s"###### ${num} ######")
+          case _                 => Seq[String](line)
+      })
+      .flatten
 
+  def MARKDOWN: Lines => Lines = (s) => {
+    s.foldLeft(
+        (s, false): (Lines, Boolean)
+      )(
+        (t, line) => {
+          val omit: Boolean = if (line.startsWith("---")) (!t._2) else t._2
+          (if (omit) t._1 else (t._1 :+ line), omit)
+        }
+      )
+      ._1
 
-  val tractatus:Lines=>Lines =(lines: Seq[String])=>
-    lines.map(line=>line match{
-      case R1(aline:String) => Seq[String]("\n",aline)
-      case R11(aline) => Seq[String](aline)
-      case R2(num)   => Seq[String](s"# ${num} #")
-      case R3(num)   => Seq[String](s"## ${num} ##")
-      case R4(num)   => Seq[String](s"### ${num} ###")
-      case R5(num)   => Seq[String](s"#### ${num} ####")
-      case R6(num)   => Seq[String](s"##### ${num} #####")
-      case R7(num)   => Seq[String](s"###### ${num} ######")
-      case _ => Seq[String](line)
-    }).flatten
+  }
 
-def ALL:Lines => Lines =  (s) => s
+  def ALL: Lines => Lines = (s) => s
 
-  def filterMarker(marker:String)(in: Seq[String]):Seq[String] =
-    in.dropWhile(!_.matches(marker)).
-      drop(1).
-      takeWhile(!_.matches(marker))
+  def filterMarker(marker: String)(in: Seq[String]): Seq[String] =
+    in.dropWhile(!_.matches(marker)).drop(1).takeWhile(!_.matches(marker))
 
 }
